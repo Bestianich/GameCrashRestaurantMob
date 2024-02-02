3067f7a866252c5cd2f1a6e1e62b2acdfe974b4f
Bestianich <davidefedeli8899@gmail.com>
2024-02-02 14:51:09 +0100

    added Initial FIle

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private float speed = 5;        
+    private 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void MoveUp(){
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
